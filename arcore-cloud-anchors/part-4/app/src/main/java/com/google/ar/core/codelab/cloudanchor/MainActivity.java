531b386c61911f5fd48b79373bd70a701ff527b3
Asad Ullah Naweed <anaweed@google.com>
2018-05-07 11:45:07 -0700

    More bug fixes for arcore-cloud-anchors

--- arcore-cloud-anchors/part-4/app/src/main/java/com/google/ar/core/codelab/cloudanchor/MainActivity.java
+++ arcore-cloud-anchors/part-4/app/src/main/java/com/google/ar/core/codelab/cloudanchor/MainActivity.java
@@ -1,17 +1,3 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /*
  * Copyright 2018 Google Inc. All Rights Reserved.
  *
@@ -190,9 +176,9 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
                   storageManager.storeUsingShortCode(shortCode, anchor.getCloudAnchorId());
                   snackbarHelper.showMessageWithDismiss(
                       this, "Anchor hosted successfully! Cloud Short Code: " + shortCode);
-                  appAnchorState = AppAnchorState.HOSTED;
                 }
               });
+          appAnchorState = AppAnchorState.HOSTED;
         }
       } else if (appAnchorState == AppAnchorState.RESOLVING) {
         // If the app is waiting for a resolving action to complete.
