f7990049174e64a62b3ae864c1f01552d4beeed5
Kalyan Reddy <gkal@google.com>
2018-05-07 23:31:00 -0700

    Added starter project for ML Kit custom model codelab

--- mlkit-android/custom-model/final/app/src/main/java/com/google/firebase/codelab/mlkit_custommodel/MainActivity.java
+++ mlkit-android/custom-model/final/app/src/main/java/com/google/firebase/codelab/mlkit_custommodel/MainActivity.java
@@ -56,7 +56,6 @@ import java.nio.ByteOrder;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
